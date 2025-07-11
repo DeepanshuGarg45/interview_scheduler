b346b759499546474e996eea1e6796c2e874c8db
DeepanshuGarg45 <112759328+DeepanshuGarg45@users.noreply.github.com>
2025-07-11 12:31:16 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CandidateList from './components/CandidateList';
+
+function App() {
+  return (
+    <div style={{ padding: '1rem' }}>
+      <h1>Interview Scheduler</h1>
+      <CandidateList />
+    </div>
+  );
+}
+
+export default App;
