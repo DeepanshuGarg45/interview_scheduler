7162218115c1fc3d3585121f40f40ff2df8dc16a
DeepanshuGarg45 <112759328+DeepanshuGarg45@users.noreply.github.com>
2025-07-11 12:31:22 +0530

    Create index.js

---
+++ frontend/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { CandidateProvider } from './context/CandidateContext';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <CandidateProvider>
+      <App />
+    </CandidateProvider>
+  </React.StrictMode>
+);
